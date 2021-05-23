ebfdc7159091de0edb630ca0c28ddf8a2000c5c7
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-05-23 13:21:30 +0100

    added bookmarks, lists controller + models

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,29 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(strong_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def strong_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
