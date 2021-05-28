d832443c9db359aece292bbe49f9e526a15eacd2
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-05-28 16:20:02 +0100

    made changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,13 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+document.addEventListener('turbolinks:load', () => {
+  initSelect2();
+  initTyped();
+  AOS.init();
+  initStarRating();
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
